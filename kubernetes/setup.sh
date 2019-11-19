fb456779cc3d99938f0b36c7eb6f4db6b22830ac
Philippe Stemberger <philippe.stemberger@snyk.io>
2019-11-19 14:43:46 +0000

    snyk k8s config

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -24,3 +24,7 @@ tilt up
 
 # To access the dashboard run the following command then go to http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/
 # kubectl proxy
+
+# To get snyk up and running
+# I couldn't get this to work in Tile because I need a different namespace and couldn't work out how to get it to switch context
+# helm upgrade --install snyk-monitor snyk-charts/snyk-monitor --namespace snyk-monitor --set clusterName="Production cluster"
